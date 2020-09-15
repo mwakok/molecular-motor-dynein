88ee80a6c371db057575b344adbaf18f8324763d
Maurits Kok <mwakok@users.noreply.github.com>
2020-09-15 23:25:19 +0200

    Add docstring to update_state.py

--- motor/update_state.py
+++ motor/update_state.py
@@ -1,20 +1,36 @@
-import numpy as np
+import numpy
 
 
 def update_state(event, state):
+    """ Update the state of the lattice
+
+    Parameters
+    ----------
+    event : int
+        type of event that occurs
+    state : numpy.ndarray
+        current lattice state
+
+    Returns
+    -------
+    state : numpy.ndarray
+        array that contains the updated lattice state
+
+    """
+
     if event == 0:  # Attachment event
-        pos = np.nonzero(state == 0)[0]
-        index = np.random.choice(pos)
+        pos = numpy.nonzero(state == 0)[0]
+        index = numpy.random.choice(pos)
         state[index] += 1
     elif event == 1:  # Detachment event lattice
-        pos = np.nonzero(state[:-1] == 1)[0]
-        index = np.random.choice(pos)
+        pos = numpy.nonzero(state[:-1] == 1)[0]
+        index = numpy.random.choice(pos)
         state[index] -= 1
     elif event == 2:  # Detachment event lattice end
         state[-1] -= 1
     elif event == 3:  # Forward hopping event
-        pos = np.nonzero((state[1:] - state[:-1]) == -1)[0]
-        index = np.random.choice(pos)
+        pos = numpy.nonzero((state[1:] - state[:-1]) == -1)[0]
+        index = numpy.random.choice(pos)
         state[index] -= 1
         state[index + 1] += 1
 
