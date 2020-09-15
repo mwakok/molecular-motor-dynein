955a1f63397f7b87a1dc86d6d17f0d74f53cee1a
Maurits Kok <mwakok@users.noreply.github.com>
2020-09-15 23:27:42 +0200

    Add docstring to update_propensity.py

--- motor/calculate_propensity.py
+++ motor/calculate_propensity.py
@@ -2,6 +2,23 @@ import numpy as np
 
 
 def propensity(rates, lattice):
+    """ Calculate the propensity for each event type
+
+    Parameters
+    ----------
+    rates : int
+        kinetic rates as defined in sim_parameters.py
+    lattice : numpy.ndarray
+        current lattice state
+
+    Returns
+    ------
+    pp : numpy.ndarray
+        propensities for each event type
+
+
+    """
+
     num_event_1 = np.count_nonzero(lattice == 0)  # Empty sites
     num_event_2 = np.count_nonzero(lattice[:-1] == 1)  # Occupied sites
     num_event_3 = lattice[-1]  # Lattice end
@@ -11,23 +28,3 @@ def propensity(rates, lattice):
         rates, [num_event_1, num_event_2, num_event_3, num_event_4])
 
     return pp
-
-
-def update_state(event, state):
-    if event == 0:  # Attachment event
-        pos = np.nonzero(state == 0)[0]
-        index = np.random.choice(pos)
-        state[index] += 1
-    elif event == 1:  # Detachment event lattice
-        pos = np.nonzero(state[:-1] == 1)[0]
-        index = np.random.choice(pos)
-        state[index] -= 1
-    elif event == 2:  # Detachment event lattice end
-        state[-1] -= 1
-    elif event == 3:  # Forward hopping event
-        pos = np.nonzero((state[1:] - state[:-1]) == -1)[0]
-        index = np.random.choice(pos)
-        state[index] -= 1
-        state[index + 1] += 1
-
-    return state
