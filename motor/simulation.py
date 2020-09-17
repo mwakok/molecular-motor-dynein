5aa6969976ea53c58dc3ca1378b031f41521a5f6
Maurits Kok <mwakok@users.noreply.github.com>
2020-09-17 14:27:05 +0200

    Change variable names to conform with PEP8

--- motor/simulation.py
+++ motor/simulation.py
@@ -1,5 +1,5 @@
 import numpy as np
-from .calculate_propensity import propensity
+from .calculate_propensity import calculate_propensity
 from .update_state import update_state
 
 
@@ -13,36 +13,42 @@ def gillespie(simPa):
 
     Returns
     -------
-    lattice_cc : 
+    collect_lattice : numpy.ndarray
 
-    time_cc :
+    collect_timepoints : list
 
     Notes
     -----
 
 
     """
-    rates = [simPa.concentration*simPa.k_on,
-             simPa.k_off, simPa.k_off_end, simPa.k_hop]
+    kinetic_rates = [
+        simPa.concentration * simPa.k_on,
+        simPa.k_off,
+        simPa.k_off_end,
+        simPa.k_hop,
+    ]
 
     # Initialize simulation counters and output
     counter = 0  # simulation iteration
-    tt = 0  # time
-    lattice = np.zeros(simPa.length)  # start simulation with an empty lattice
-    lattice_cont = []
-    tt_cont = []
-    rand_nums = np.random.rand(2, simPa.iter_max)   # Pre-draw random numbers
+    elapsed_simulation_time = 0  # time
+    lattice_state = np.zeros(simPa.length)  # start simulation with an empty lattice
+    collect_lattice_states = []
+    collect_timepoints = []
+    rand_nums = np.random.rand(2, simPa.iter_max)  # Pre-draw random numbers
 
     # Run Gillespie simulation until "simPa.time_max"
-    while simPa.iter_max > counter and simPa.time_max+1 > tt:
+    while simPa.iter_max > counter and simPa.time_max + 1 > elapsed_simulation_time:
 
-        pp = propensity(rates, lattice)  # Calculate probability for all events
-        delta_t = -np.log(rand_nums[0][counter]) / \
-            pp.sum()  # Draw waiting time
+        # Calculate probability for all events
+        propensity_event_type = calculate_propensity(kinetic_rates, lattice_state)
+
+        # Draw waiting time
+        dt = -np.log(rand_nums[0][counter]) / propensity_event_type.sum()
 
         # Construct valid events
-        valid_inds = pp > 0  # Find the possible events
-        valid_pp = pp[valid_inds]  # Include only valid events
+        valid_inds = propensity_event_type > 0  # Find the possible events
+        valid_pp = propensity_event_type[valid_inds]  # Include only valid events
         valid_changes = np.nonzero(valid_inds)[0]
 
         # Stop current simulation if no valid changes are available
@@ -50,24 +56,28 @@ def gillespie(simPa):
             break
 
         selection_interval = valid_pp.cumsum()  # Construct intervals
-        selection_interval = selection_interval/selection_interval[-1]
-        selected_ind = np.nonzero(selection_interval > rand_nums[1][counter])[
-            0][0]   # Select interval
-        # Update lattice configuration based on chosen interval
-        lattice = update_state(valid_changes[selected_ind], lattice)
+        selection_interval = selection_interval / selection_interval[-1]
+        selected_ind = np.nonzero(selection_interval > rand_nums[1][counter])[0][0]
 
-        tt += delta_t
+        # Update lattice configuration based on chosen interval
+        lattice_state = update_state(valid_changes[selected_ind], lattice_state)
+
+        elapsed_simulation_time += dt
         counter += 1
 
         # Apply sampling if applicable
         if simPa.frame_time > 0:
-            while len(lattice_cont) < np.floor(tt/simPa.frame_time) and len(lattice_cont) <= simPa.time_max:
-                lattice_cont.append(lattice.copy())
-                tt_cont.append(tt)
+            while (
+                len(collect_lattice_states)
+                < np.floor(elapsed_simulation_time / simPa.frame_time)
+                and len(collect_lattice_states) <= simPa.time_max
+            ):
+                collect_lattice_states.append(lattice_state.copy())
+                collect_timepoints.append(elapsed_simulation_time)
 
     # Collect final state if sampling is off
     if simPa.frame_time == 0:
-        lattice_cont = lattice
-        tt_cont = tt
+        collect_lattice_states = lattice_state
+        collect_timepoints = elapsed_simulation_time
 
-    return lattice_cont, tt_cont
+    return collect_lattice_states, collect_timepoints
