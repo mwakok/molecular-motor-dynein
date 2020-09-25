ba8f2ec7f96680518f68dbdf967f125da2a276e1
Maurits Kok <mwakok@users.noreply.github.com>
2020-09-25 09:25:36 +0200

    Update test_update_state to account for string input of event type

--- tests/test_update_state.py
+++ tests/test_update_state.py
@@ -4,7 +4,7 @@ import numpy
 
 def test_update_state_attachment():
     # Test motor landing on lattice
-    selected_event = 0
+    selected_event = "attach"
     lattice_state = numpy.zeros(5)
     lattice_state_new = update_state(selected_event, lattice_state)
     assert numpy.sum(lattice_state_new) == 1
@@ -12,7 +12,7 @@ def test_update_state_attachment():
 
 def test_update_state_detachment():
     # Test motor detachment from lattice
-    selected_event = 1
+    selected_event = "detach"
     lattice_state = numpy.ones(5)
     lattice_state_new = update_state(selected_event, lattice_state)
     assert numpy.sum(lattice_state_new) == 4
@@ -20,7 +20,7 @@ def test_update_state_detachment():
 
 def test_update_state_detachment_end():
     # Test motor detachment of lattice end
-    selected_event = 2
+    selected_event = "detach_end"
     lattice_state = numpy.ones(5)
     lattice_state_new = update_state(selected_event, lattice_state)
     expected_lattice = numpy.append(numpy.ones(4), 0)
@@ -29,7 +29,7 @@ def test_update_state_detachment_end():
 
 def test_update_state_forward_hopping():
     # Test forward hopping on lattice
-    selected_event = 3
+    selected_event = "forward_hop"
     lattice_state = numpy.array([1, 0, 0, 0, 0])
     lattice_state_new = update_state(selected_event, lattice_state)
     expected_lattice = numpy.array([0, 1, 0, 0, 0])
