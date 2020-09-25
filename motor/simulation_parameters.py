c02a98442bd179d196ff66e3af41d3a610123153
Jose Carlos Urra <jose.urra86@gmail.com>
2020-09-25 15:05:54 +0200

    formatted dictionary for more readability

--- motor/simulation_parameters.py
+++ motor/simulation_parameters.py
@@ -2,30 +2,32 @@ import numpy
 
 
 # Dictionary to store all simulation parameters
-simParameters = {}
 
+simParameters = {
+    
 # Store data and figures: True or False
-simParameters["record_data"] = False
-simParameters["num_sims"] = 200  # Number of simulations per scan condition
-simParameters["scan"] = False  # True (vary lattice size) or False
+"record_data": False,
+"num_sims": 200,  # Number of simulations per scan condition
+"scan": False,  # True (vary lattice size) or False
 
 # Determine type of dynein: 'WT' (Wild-Type) or 'SP' (Super-Processive)
-simParameters["type"] = "SP"
-simParameters["concentration"] = 2.5  # Dynein concentration in nM
+"type": "SP",
+"concentration": 2.5 , # Dynein concentration in nM
 
 # Basic lattice unit is size of motor step, for dynein the step size is 24 nm
-simParameters["step_size"] = 24
-simParameters["length"] = 100  # Length lattice in hopping units
-simParameters["length_range"] = numpy.arange(40, 301, 10)  # length range to scan
+"step_size": 24,
+"length": 100, # Length lattice in hopping units
+"length_range": numpy.arange(40, 301, 10),  # length range to scan
 
 # Maximum number of iterations per condition
-simParameters["iter_max"] = 100000
-simParameters["time_max"] = 300  # Max simulated time (in seconds)
+"iter_max": 100000,
+"time_max": 300,  # Max simulated time (in seconds)
 
 # Frame time to sample the lattice configuration (in seconds).
 # If 0, then only the final state will be saved and no kymographs will be generated
-simParameters["frame_time"] = 1
+"frame_time": 1,
 
+}
 
 if simParameters["type"] == "WT":  # Wild-type dynein
     simParameters["k_on"] = 0.0025  # Attachment rate [/nM/sec]
